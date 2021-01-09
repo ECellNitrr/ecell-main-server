ebe65fcfdae033dc87d94dbe74d0cc83bbc689e0
naveennvrgup <naveennvrgup@gmail.com>
2021-01-09 12:15:59 +0530

    cleanup: sponsors and old data transfer scripts

--- events/urls.py
+++ events/urls.py
@@ -2,13 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.views.decorators.csrf import csrf_exempt
-from utils.events_trans import data_transfer
 
 
 urlpatterns = [
     path('list/<int:year>/',views.get_events,name="get_events"),
     path('noticeboard/',views.NoticeBoardListView.as_view(),name="notices_list"),
     path('register/<int:id>/', csrf_exempt(views.event_register), name='eventregister'),
-    path('data_transfer/', data_transfer),
     path('unregister/<int:id>/', csrf_exempt(views.event_unregister), name='eventunregister'),
 ]
--- speakers/urls.py
+++ speakers/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from utils.speaker_trans import data_transfer
 
 urlpatterns = [
     path('full_list/', views.get_speakers_list),
-    path('data_transfer/', data_transfer)
-    ]
+]
