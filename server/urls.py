1d21a3b8d843bf36e59d7e3a67e78a1b362649ad
Ankur Kunal <mrankur801@gmail.com>
2021-01-10 18:18:00 +0530

    added swagger doc

--- server/urls.py
+++ server/urls.py
@@ -7,8 +7,22 @@ from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
 from android_app.views import latest_build
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="E-Cell Main Server API",
+      default_version='v1',
+      description="Swagger Documentation for APIs used in the Official Website of E-Cell NITRR",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    path('docs/',schema_view.with_ui('swagger',cache_timeout=0), name = 'schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('events/', include('events.urls')),
