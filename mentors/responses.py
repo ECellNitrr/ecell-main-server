603774381206a7907ec38a207909069e57f08c36
Akhil Soni <58397226+akhil-maker@users.noreply.github.com>
2021-01-11 18:53:33 +0530

    Add response.py

---
+++ mentors/responses.py
@@ -0,0 +1,20 @@
+get_mentors_200 ={
+  "message": "Mentors Fetched successfully.",
+  "data": [
+    {
+        "id": 0,
+        "name": "string",
+        "email": "string",
+        "contact": "string",
+        "detail": "string",
+        "description": "string",
+        "profile_pic_url": "string",
+        "year": 0,
+        "flag": True
+    }
+  ]
+}
+
+mentors_not_found_404 = {
+    "detail": "Mentors couldn't be fetched."
+}
