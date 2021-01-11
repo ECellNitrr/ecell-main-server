abb32ba63802e2ffb3d2f1d5381ff113b98db8ba
Akhil Soni <58397226+akhil-maker@users.noreply.github.com>
2021-01-11 11:47:04 +0530

    Update views.py

--- mentors/views.py
+++ mentors/views.py
@@ -1,35 +1,24 @@
-# TODO: remove unused imports
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from .models import Mentor
 from .serializers import MentorSerializer, MentorListSerializer
 from decorators import ecell_user
 from django.http import HttpResponse
 import csv
+from rest_framework import status, generics, filters
 
+class MentorView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Mentor.objects.all()
+    serializer_class = MentorListSerializer
 
-# TODO: simplify with drf
-@api_view(['GET', ])
-def get_mentors(request, year):
-
-    res_message = ""
-    res_status = ""
-    res_data = []
-
-    mentors = Mentor.objects.filter(year=year, flag=True)
-    if len(mentors) > 0:
-        res_data = MentorListSerializer(
-            mentors, many=True, context={
-                'request': request}).data
-        res_message = "Mentors Fetched successfully."
-        res_status = status.HTTP_200_OK
-    else:
-        res_message = "Mentors Couldn't be fetched"
-        res_status = status.HTTP_404_NOT_FOUND
-
-    return Response({
-        "message": res_message,
-        "data": res_data
-    }, status=res_status)
+    def list(self, request, year):
+        queryset = Mentor.objects.filter(year=year, flag=True)
+        serializer = MentorListSerializer(queryset, many=True)
+        data = serializer.data
+        if queryset.count() > 0:
+            return Response({"message":"Mentors Fetched successfully.", "data":data}, status.HTTP_200_OK)
+        else:
+            return Response({"message":"Mentors Couldn't be fetched"}, status.HTTP_404_NOT_FOUND)
