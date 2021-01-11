9321e500313bc203e9a724bcbb4017d8bcab40ca
Akhil Soni <58397226+akhil-maker@users.noreply.github.com>
2021-01-11 18:57:00 +0530

    Remove unused imports

--- mentors/urls.py
+++ mentors/urls.py
@@ -1,9 +1,6 @@
-from django.contrib import admin
 from django.urls import path
-from .models import Mentor
 from . import views
-from .serializers import MentorListSerializer
 
 urlpatterns = [
-    path('list/<year>', views.MentorView.as_view(), name='mentor_list')
+    path('list/<year>/', views.MentorView.as_view(), name='mentor_list')
 ]
