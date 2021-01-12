0e388558c7a7757a68b2b411a797408359f6123b
sahil9001 <sahilsilare@gmail.com>
2021-01-13 01:26:24 +0530

    refactor: Refactored the event unregister view.

--- events/views.py
+++ events/views.py
@@ -1,15 +1,19 @@
-from rest_framework.decorators import api_view
+# TODO: clean imports
+from rest_framework import status
+from rest_framework import response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
 from .models import Event, EventRegister
 from .serializers import *
 from decorators import ecell_user
-from rest_framework import status, generics
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework.permissions import AllowAny,IsAuthenticated
 from utils.swagger import set_example
-from rest_framework.views import APIView
 from . import responses
+# from django.utils.six.moves.urllib.parse import urlsplit
+import csv
 
 class EventView(generics.ListAPIView):
     permission_classes = [AllowAny, ]
@@ -67,32 +71,33 @@ class EventRegisterView(APIView):
         return Response(responses.user_unauthorized_401,status.HTTP_401_UNAUTHORIZED)
 
 # TODO: simplify with drf
-@api_view(['POST', ])
-@ecell_user
-def event_unregister(request, id):
-    u = request.ecelluser
-    if u:
-        try:
-            e = Event.objects.get(id=id)
-        except:
-            res_message="Event does not exist"
-            res_status=status.HTTP_404_NOT_FOUND   
-        else:
+class EventUnregisterView(APIView):
+    permission_classes = [IsAuthenticated]
+    @swagger_auto_schema(
+        operation_id="eventunregister",
+        responses={
+            '200' : set_example(responses.event_registration_deleted_200),
+            '403' : set_example(responses.user_forbidden_403),
+            '404' : set_example(responses.event_does_not_exist_404,responses.event_not_registered_404)
+        }
+    )
+    def post(request, id):
+        user = request.user
+        if user:
             try:
-                reg = EventRegister.objects.filter(user = u, event= e)  
+                event = Event.objects.get(id=id)
             except:
-                res_message= "Event not registered"
-                res_status=status.HTTP_404_NOT_FOUND
+                return Response(responses.event_does_not_exist_404,status.HTTP_404_NOT_FOUND) 
             else:
-                res_message="Registration deleted successfully"
-                reg.delete()
-                res_status=status.HTTP_200_OK
-    
-    else:
-        res_message = "Login to continue"
-    return Response({
-        "message": res_message
-    }, status=res_status)
+                try:
+                    reg = EventRegister.objects.filter(user = user, event= event)  
+                except:
+                    return Response(responses.event_not_registered_404,status.HTTP_404_NOT_FOUND)
+                else:
+                    reg.delete()
+                    return Response(responses.event_registration_deleted_200,status.HTTP_200_OK)
+        
+        return Response(responses.user_forbidden_403,status.HTTP_403_FORBIDDEN)
 
 
 # TODO: in next meeting report why this exsits
