630a7c434c899c0f27a4db19d8829867829fbe9f
Akhil Soni <58397226+akhil-maker@users.noreply.github.com>
2021-01-13 14:34:45 +0530

    refactor: get mentors API (#15)
    * Update urls.py
    
    * Update views.py
    
    * Add response.py
    
    * Update with swagger docs and remove unused imports
    
    * Remove unused imports
    
    * Update views.py
    
    * Update responses.py
    
    * Update views.py
    
    * Update views.py
    
    * Update views.py

---
+++ mentors/responses.py
@@ -0,0 +1,20 @@
+get_mentors_200 ={
+  "message": "Mentors Fetched successfully.",
+  "data": [
+    {
+        "id": 0,
+        "name": "test",
+        "email": "test@tmail.com",
+        "contact": "9999999999",
+        "detail": "test_detail",
+        "description": "test_desc",
+        "profile_pic_url": "test_profile_pic",
+        "year": 9999,
+        "flag": True
+    }
+  ]
+}
+
+mentors_not_found_404 = {
+    "message": "Mentors couldn't be fetched."
+}
