0cb36797268a99b83cd9635c6060e321c991ece1
Priyanshi Sharma <priyanshi417@gmail.com>
2021-01-14 23:48:22 +0530

    docs: comment hard to understand area

--- events/views.py
+++ events/views.py
@@ -50,18 +50,22 @@ class EventRegisterView(APIView):
         try:
             event = Event.objects.get(id=id)
         except:
-            return Response(responses.event_does_not_exist_404, status.HTTP_404_NOT_FOUND)  
+            return Response(responses.event_does_not_exist_404, status.HTTP_404_NOT_FOUND)
+
         if user.verified:
+            #Checking if user is already registered
             try:
-                registeredUser = EventRegister.objects.get(user=user,event=event)
+                EventRegister.objects.get(user=user,event=event)
             except:
+                #Registering the user if not registered
                 eventregister.user = user
                 eventregister.event = event
                 eventregister.save()
                 return Response(responses.event_registration_201,status.HTTP_201_CREATED)
-            return Response(responses.user_already_registered_event_200,status.HTTP_200_OK)    
-        else:
-            return Response(responses.user_unauthorized_401,status.HTTP_401_UNAUTHORIZED)
+
+            return Response(responses.user_already_registered_event_200,status.HTTP_200_OK)
+
+        return Response(responses.user_unauthorized_401,status.HTTP_401_UNAUTHORIZED)
 
 # TODO: simplify with drf
 @api_view(['POST', ])
