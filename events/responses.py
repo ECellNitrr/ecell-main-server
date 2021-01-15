6ae7bf0091c7d1677cb9592972ca05c2ad042edd
sahil9001 <sahilsilare@gmail.com>
2021-01-15 20:49:28 +0530

    Reverted few changes

--- events/responses.py
+++ events/responses.py
@@ -1,15 +1,42 @@
+get_events_200 ={
+  "message": "Events Fetched successfully.",
+  "data": [
+    {
+    "id":0,
+    "name": "test",
+    "venue" : "test_venue",
+    "date" : "2020-09-29T15:21:48.828+00:00",
+    "time" : "test_time",
+    "details": "test_details",
+    "details_html" : "test.html",
+    "cover_pic" : "test_cover.jpeg",
+    "icon" : "test_icon.png",
+    "email": "test@email.com",
+    "flag" :True,
+    "year" :2019,
+    "ecell_user" :3,
+    "created_at":"2020-09-29T15:21:48.828+00:00",
+    "modified_at":"2020-09-29T15:21:48.828+00:00"
+    }
+  ]
+}
+
+events_not_found_404 = {
+    "message": "Events couldn't be fetched."
+}
+
+event_registration_201 = {
+    "message":"Registration Successful"
+}
+
 event_does_not_exist_404 = {
-    "message" : "Event does not exist"
+  "message": "Registration Failed. Event does not exist."
 }
 
-event_not_registered_404 = {
-    "message" : "Event not registered"
+user_unauthorized_401 = {
+  "message":"Please follow OTP verification before registering"
 }
 
-event_registration_deleted_200 = {
-    "message" : "Registration deleted successfully"
-}
-
-user_forbidden_403 = {
-    "message" : "Login to continue"
+user_already_registered_event_200 = {
+  "message":"You've already registered"
 }
