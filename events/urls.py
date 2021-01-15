68fd35798ccce4d185ad3f3c83393c06b49e4644
sahil9001 <sahilsilare@gmail.com>
2021-01-15 20:50:37 +0530

    Added URL Route

--- events/urls.py
+++ events/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('list/<year>/',views.EventView.as_view(),name="events_list"),
     path('noticeboard/',views.NoticeBoardListView.as_view(),name="notices_list"),
     path('register/<id>/', csrf_exempt(views.EventRegisterView.as_view()), name='event_register'),
-    path('unregister/<int:id>/', csrf_exempt(views.event_unregister), name='eventunregister'),
+    path('unregister/<int:id>/', csrf_exempt(views.EventUnregisterView.as_view()), name='eventunregister'),
 ]
