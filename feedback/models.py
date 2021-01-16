a189f313689f6a7e2d634ad86910dc7b2cbe5e3c
sahil9001 <sahilsilare@gmail.com>
2021-01-16 18:23:33 +0530

    refactor: Removed CharField and added EmailField

--- feedback/models.py
+++ feedback/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Feedback(models.Model):
     name = models.CharField(max_length=100)
-    email = models.CharField(max_length=256)
+    email = models.EmailField(max_length=100)
     message = models.TextField()
 
     def __str__(self):
