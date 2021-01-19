5e2a3a908510893013754d9737a789a139fcb523
Sahil Silare <32628578+sahil9001@users.noreply.github.com>
2021-01-19 14:31:56 +0530

    refactor: Refactored feedback POST request (#16)
    * refactor: Refactored feedback POST request
    
    * refactor: Added responses and refactored views
    
    * Update feedback/urls.py
    
    Co-authored-by: Ansh Srivastava <ansh_srtv@outlook.com>
    
    * Renamed serializer and used responses
    
    * Removed else as it was redundant
    
    * refactor: Removed CharField and added EmailField
    
    Co-authored-by: Ansh Srivastava <ansh_srtv@outlook.com>

---
+++ feedback/responses.py
@@ -0,0 +1,10 @@
+feedback_created_201 = {
+    "message" : "Feedback posted successfully"
+}
+
+feedback_invalid_400 = {
+    "message" : [
+        "Error in field: name- Ensure this field has no more than 100 characters.",
+        "Error in field: email- Ensure this field has no more than 256 characters.",
+    ]
+}
