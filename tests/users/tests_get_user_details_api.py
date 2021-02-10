ad198fcde7fe2819517d635171e5dc4280250244
sahil9001 <sahilsilare@gmail.com>
2021-02-10 15:28:49 +0530

    fix:Added AuthAPITestCase files and inherited in different test cases

--- tests/users/tests_get_user_details_api.py
+++ tests/users/tests_get_user_details_api.py
@@ -1,41 +1,32 @@
 from django.http import response
-from users.models import CustomUser
 from rest_framework.test import APITestCase, APIClient
+from tests.AuthAPITestCase import AuthAPITestCase
 from rest_framework import status
 
-class GetUserDetailsTestCase(APITestCase):
+class GetUserDetailsTestCase(AuthAPITestCase):
 
     get_user_details_api = "/users/get_user_details/"
     
     def setUp(self):
-        self.email = "crash.test.dummy@gmail.com"
-        self.password = "test.modelx"
-
-        self.user = CustomUser.objects.create_user(
-            email = self.email,
-            username = self.email,
-            first_name = "Crash",
-            last_name = "Test",
-            contact = "+919999999999",
-            password = self.password,
-        )
-
-    def create_auth_client(self):
-        login_api = "/users/login/"
-        login_payload = {
-            'email': self.email,
-            'password': self.password
-        }
-
-        auth_client = APIClient()
-        login_response = auth_client.post(login_api, login_payload)
-        auth_token = login_response.data['token']
-        auth_client.credentials(HTTP_AUTHORIZATION=auth_token)
-        return auth_client
+        super(GetUserDetailsTestCase,self).setUp()
 
     def test_success_authorized_user(self):
         auth_client = self.create_auth_client()
         response = auth_client.get(self.get_user_details_api)
+        data_user = {
+            'first_name': 'Crash',
+            'last_name': 'Test',
+            'email': self.auth_user_email,
+            'verified': False,
+            'contact': '+919999999999',
+            'bquiz_score': 0,
+            'user_type': 'GST',
+            'linkedin': None,
+            'facebook': None,
+            'applied': False,
+            'id': 2
+        }
+        self.assertEqual(response.data, data_user)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
     
     def test_fail_unauthorized_user(self):
@@ -43,58 +34,26 @@ class GetUserDetailsTestCase(APITestCase):
         response = unauth_client.get(self.get_user_details_api)
         self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
 
-    def tearDown(self):
-        self.user.delete()
-
-class UserVerifiedTestCase(APITestCase):
+ 
+class UserVerifiedTestCase(AuthAPITestCase):
     is_user_verified_api = "/users/is_user_verified/"
     
     def setUp(self):
-        self.email = "crash.test.dummy@gmail.com"
-        self.password = "test.modelx"
-
-        self.user = CustomUser.objects.create_user(
-            email = self.email,
-            username = self.email,
-            first_name = "Crash",
-            last_name = "Test",
-            contact = "+919999999999",
-            password = self.password,
-            otp = "123456"
-        )
-
-    def create_auth_client(self):
-        login_api = "/users/login/"
-        login_payload = {
-            'email': self.email,
-            'password': self.password
-        }
-        auth_client = APIClient()
-        login_response = auth_client.post(login_api, login_payload)
-        auth_token = login_response.data['token']
-        auth_client.credentials(HTTP_AUTHORIZATION=auth_token)
-        return auth_client
-
-    def create_verified_auth_client(self):
-        verify_otp_api = "/users/verify_otp/"
-        auth_client = self.create_auth_client()
-        data = {
-            "otp" : self.user.otp
-        }
-        response = auth_client.post(verify_otp_api,data)
-        return auth_client
+        super(UserVerifiedTestCase,self).setUp()
     
     #Verified auth client pass 
     def test_pass_verified_user(self):
         verified_auth_client = self.create_verified_auth_client()
         response = verified_auth_client.get(self.is_user_verified_api)
         self.assertEqual(response.data['verified'],True)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
 
     #Logged in user but not verified
     def test_fail_not_verified(self):
         auth_client = self.create_auth_client()
         response = auth_client.get(self.is_user_verified_api)
         self.assertEqual(response.data['verified'],False)
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
 
     #Unauthorized user
     def test_fail_not_authorized(self):
@@ -102,40 +61,21 @@ class UserVerifiedTestCase(APITestCase):
         response = unauth_client.get(self.is_user_verified_api)
         self.assertEqual(response.status_code,status.HTTP_401_UNAUTHORIZED)
 
-    def tearDown(self):
-        self.user.delete()
 
-class CAApprovalTestCase(APITestCase):
+class CAApprovalTestCase(AuthAPITestCase):
     request_ca_approval_api = "/users/request_ca_approval/"
     
     def setUp(self):
-        self.email = "crash.test.dummy@gmail.com"
-        self.password = "test.modelx"
+        super(CAApprovalTestCase,self).setUp()
 
-        self.user = CustomUser.objects.create_user(
-            email = self.email,
-            username = self.email,
-            first_name = "Crash",
-            last_name = "Test",
-            contact = "+919999999999",
-            password = self.password,
-        )
-
-    def create_auth_client(self):
-        login_api = "/users/login/"
-        login_payload = {
-            'email': self.email,
-            'password': self.password
-        }
-        auth_client = APIClient()
-        login_response = auth_client.post(login_api, login_payload)
-        auth_token = login_response.data['token']
-        auth_client.credentials(HTTP_AUTHORIZATION=auth_token)
-        return auth_client
-
-    #Auth user successful approval
+    #Auth user successful application
     def test_pass_auth_user(self):
         auth_client = self.create_auth_client()
+        response = auth_client.get(self.request_ca_approval_api)
+        #Successfully applied
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+        #Already applied
         response = auth_client.get(self.request_ca_approval_api)
         self.assertEqual(response.status_code,status.HTTP_200_OK)
 
