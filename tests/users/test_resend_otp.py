59fc0e20eefe652f9d1b7a72dde5aa554ab739d9
Akhil Soni <58397226+akhil-maker@users.noreply.github.com>
2021-02-15 22:53:18 +0530

    Update docs and setup
    Creating user and get auth_client from AuthAPITestCase and changes in doc, replacing them from their position to position after function declaration.

--- tests/users/test_resend_otp.py
+++ tests/users/test_resend_otp.py
@@ -1,56 +1,27 @@
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APIClient
 from rest_framework import status
-from users.models import CustomUser
+from tests.AuthAPITestCase import AuthAPITestCase
 
+class ResendOTPTestCase(AuthAPITestCase):
 
-class ResendOTPTestCase(APITestCase):
-    '''
-        Creating a user for resend_otp api
-    '''
     def setUp(self):
-        self.email = "test@gmail.com"
-        self.password = "password"
+        '''
+             Creating a user for resend_otp api using AuthAPITestCAse
+        '''
+        super(ResendOTPTestCase, self).setUp()
 
-        self.user = CustomUser.objects.create_user(
-            email = self.email,
-            username = self.email,
-            first_name = "well",
-            last_name = "done",
-            contact = "+919999999999",
-            password = self.password,
-            verified = False,
-        )
-
-    '''
-        Creating an auth client
-    '''
-    def create_auth_client(self):
-        login_api = "/users/login/"
-        login_payload = {
-            "email": self.email,
-            "password": self.password
-        }
-        auth_client = APIClient()
-        login_response = auth_client.post(login_api, login_payload)
-        auth_token = login_response.data['token']
-        auth_client.credentials(HTTP_AUTHORIZATION=auth_token)
-        return auth_client
-
-    '''
-        Testing with unregistered user
-    '''
     def test_fail_unregistered_user(self):
+        '''
+             Testing with unregistered user
+        '''
         unauth_client = APIClient()
         response = unauth_client.get("/users/resend_otp/")
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    '''
-        Testing with registered user
-    '''
     def test_success_registered_user(self):
+        '''
+            Testing with registered user
+        '''
         auth_client = self.create_auth_client()
         response = auth_client.get("/users/resend_otp/")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def tearDown(self):
-        self.user.delete()
