46dd86364b3f0b52ac889b53622ffd194be82247
faltunik <niksanat8@gmail.com>
2023-03-21 20:52:41 +0530

    chngef stati dir

--- server/settings.py
+++ server/settings.py
@@ -165,7 +165,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATICFILES_DIRS = ['./frontend/build/static']
+STATICFILES_DIRS = ['./templates/']
 STATIC_URL = '/static/'
 STATIC_ROOT = './static'
 
